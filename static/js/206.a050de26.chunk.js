bea51c5c0308e551da268ed42d4238210862d7ba
nemetskii <108005949+nemetskii@users.noreply.github.com>
2023-08-03 08:51:28 +0000

    Deploying to gh-pages from @ nemetskii/goit-react-hw-05-movies@918342666b87a3ed9a2dd68bce08fe028940973f 🚀

---
+++ static/js/206.a050de26.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[206],{206:function(e,i,l){l.r(i);l(791);var n=l(87),t=l(184);i.default=function(e){var i=e.movies;return(0,t.jsx)("ul",{children:i.map((function(e){return(0,t.jsx)("li",{children:(0,t.jsx)(n.rU,{to:"".concat(null===e||void 0===e?void 0:e.id),children:null===e||void 0===e?void 0:e.original_title})},null===e||void 0===e?void 0:e.id)}))})}}}]);
+//# sourceMappingURL=206.a050de26.chunk.js.map
